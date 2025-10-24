aec43e8d6f45bae5d6dddd970e845c66acf93985
AlvesCosta <alvescosta74@gmail.com>
2025-10-24 16:12:58 -0300

    feat: initialize Spring Boot project with JPA, Security and H2

---
+++ src/main/java/com/alves/parkflow/parkflow_backend/ParkflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alves.parkflow.parkflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkflowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alves/parkflow/parkflow_backend/ParkflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alves.parkflow.parkflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
